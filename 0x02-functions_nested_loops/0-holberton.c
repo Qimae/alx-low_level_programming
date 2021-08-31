7555892b0412da1c2b49cbb21488f346f74e3e1e
Kimae <kartelbwoy@gmail.com>
2021-08-31 07:10:23 -0700

    Initial commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * main - Prints Holberton, followed by a new line
- *
- * Return: Always 0
- */
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-char *c = "Holberton";
-int i;
+	int i;
+	char p[] = "Holberton";
 
-for (i = 0; i < 9; i++)
-{
-_putchar(c[i]);
-}
-_putchar('\n');
-
-return (0);
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
+	return (0);
 }
