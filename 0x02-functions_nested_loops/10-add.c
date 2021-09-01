659e61f2bc9ac8a8fc657777b4776b37f0ac258d
Kimae <kartelbwoy@gmail.com>
2021-09-01 04:27:43 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers 
+ *
+ * @n: int value 1
+ * @j: int value 2
+ * Return: result
+ */
+int add(int n, int j)
+{
+int r;
+
+r = n + j;
+
+return (r);
+}
