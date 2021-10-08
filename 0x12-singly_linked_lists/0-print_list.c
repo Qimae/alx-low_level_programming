571ed9b35f702948836b42707013fe8c2ee4cf16
Kimae <kartelbwoy@gmail.com>
2021-10-08 01:16:21 -0700

    Initial commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,11 +5,12 @@
 /**
  * print_list - Prints all the elements of a list
  * @h: pointer to linked list
- * return: number of nodes in linked list
+ * Return: number of nodes in linked list
  */
 size_t print_list(const list_t *h)
 {
 	size_t num;
+
 	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	{
