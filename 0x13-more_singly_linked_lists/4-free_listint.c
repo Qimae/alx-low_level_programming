318bd20559a7560bad26cb194dc982f59260284b
Kimae <kartelbwoy@gmail.com>
2021-10-13 01:06:17 -0700

    Initial commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to head node
+ */
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+	head = head->next;
+	}
+}
