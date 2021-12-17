27275f9414574d25c19f5bb6c44f855db9bdb22b
Kimae <kartelbwoy@gmail.com>
2021-12-17 05:46:39 -0800

    Initial commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives the index of a key
+ * @key: key
+ * @size: size of the array hash table
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	index = hash_djb2(key);
+	return (index % size);
+}
